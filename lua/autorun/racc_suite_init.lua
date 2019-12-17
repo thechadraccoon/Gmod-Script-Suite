d96d7bd9ab9eb7af3c5f53eb4266f03259a27bfd
Racc <11398266+thechadraccoon@users.noreply.github.com>
2019-12-17 13:25:47 -0500

    Create racc_suite_init.lua

---
+++ lua/autorun/racc_suite_init.lua
@@ -0,0 +1,21 @@
+-- Please don't edit this. Use config file instead. located @ /lua/racc-script-suite/config.lua. thx <3
+if SERVER then
+  racc = {} -- Need this for the big brain stuff
+  racc.config = {} -- imagine big brain stuff without the brain to do it
+  include("racc-script-suite/config.lua") -- Call the configuration gods
+
+  if (racc.config.timer.enabled) then -- If timer is enabled 
+    include("racc-script-suite/scripts/RaccTimerServer.lua") -- call timer script
+    AddCSLuaFile("racc-script-suite/scripts/RaccTimerClient.lua")
+  else return -- else don't
+  end
+
+  if (racc.config.dynamicname.enabled) then -- if dynamic name is enabled
+    include("racc-script-suite/scripts/DynamicName.lua") -- call dynamice name script
+  else return -- else don't
+  end
+end
+
+if CLIENT then
+    include("racc-script-suite/scripts/RaccTimerClient.lua")
+end
