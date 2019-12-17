5f9a603fa4409a944b9319c15da01afa9ee1b242
Racc <11398266+thechadraccoon@users.noreply.github.com>
2019-12-17 13:14:54 -0500

    Update config.lua

--- lua/racc-script-suite/config.lua
+++ lua/racc-script-suite/config.lua
@@ -3,6 +3,8 @@
 -- Timer Stuff
 racc.config.timer.enabled = true -- do we enable the timer script or not?
 racc.config.timer.time = 300 -- how long should the timer last?
+racc.config.timer.hud = true -- enable clientside hud?
+racc.config.timer.hud.title = "timer" -- timer title (keep this under 10 characters)
 racc.config.timer.action = function() end -- Function calling or custom function either work (Ex: MapVote() or game.ConsoleCommand( "say Hi\n" ))
 
 --Dynamic server name stuff
